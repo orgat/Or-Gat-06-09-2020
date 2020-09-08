c9ad1fa3f2e1973cf8e25487c62db329fd6058a7
or.g <orgat321@gmail.com>
2020-09-08 22:28:47 +0300

    Changed http in baseurls to https

--- src/assets/api.js
+++ src/assets/api.js
@@ -1,9 +1,9 @@
 export default {
   key: '5WpMhA4b6fmIVMPiJBQieLBP6I25ZwM7',
   autocompleteBaseUrl:
-    'http://dataservice.accuweather.com/locations/v1/cities/autocomplete',
+    'https://dataservice.accuweather.com/locations/v1/cities/autocomplete',
   hourlyForecastBaseUrl:
-    'http://dataservice.accuweather.com/forecasts/v1/hourly/1hour',
+    'https://dataservice.accuweather.com/forecasts/v1/hourly/1hour',
   weeklyForecastBaseUrl:
-    'http://dataservice.accuweather.com/forecasts/v1/daily/5day',
+    'https://dataservice.accuweather.com/forecasts/v1/daily/5day',
 };
